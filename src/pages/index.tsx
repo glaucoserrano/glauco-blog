fa9534fe40c13fac62f2368717bf1926ce8b6290
Glauco Gamero <glauco.h.gamero@gmail.com>
2025-05-22 07:34:05 -0300

    feat: seção hero da landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
+import { HeroSection } from "@/components/hero-section";
+
 export default function Home() {
   return (
-    <div className="flex 
-      min-h-screen flex-col items-center 
-      justify-between
-      p-24">
-    </div>
+    <>
+      <article className="flex flex-col">
+        <HeroSection />
+      </article>
+    </>
   );
 }
