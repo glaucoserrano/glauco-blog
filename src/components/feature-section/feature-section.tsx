81e77cc8907e8e28bf2b8bf032eb3d80ff899bc0
Glauco Gamero <glauco.h.gamero@gmail.com>
2025-05-26 08:03:10 -0300

    add feature section

---
+++ src/components/feature-section/feature-section.tsx
@@ -0,0 +1,119 @@
+import Link from "next/link"
+import { Button } from "../ui/button"
+import { ArrowRight } from "lucide-react"
+import Image from "next/image"
+
+export const FeatureSection = () => {
+    return (
+        <section className="container
+            bg-gray-700 
+            grid gap-6 md:grid-cols-2 p-6 pt-8 pb-8
+            md:py-10"
+        >
+            <div className="flex flex-col gap-4 rounded-lg
+                bg-gray-500 md:p-12">
+                <span className="text-body-tag
+                 text-blue-200 bg-blue-400 px-2 py-1
+                 w-fit rounded-sm uppercase"
+            >
+                Simples
+                </span>
+                <h2 className="text-gray-100 
+                    text-heading-lg"
+                >
+                    Crie um catálogo de produtos 
+                    online em poucos minutos
+                </h2>
+            </div>
+            <div className="flex flex-col gap-4 rounded-lg
+                bg-gray-500 md:p-12">
+                <span className="text-body-tag
+                 text-blue-200 bg-blue-400 px-2 py-1
+                 w-fit rounded-sm uppercase"
+            >
+                Prático
+                </span>
+                <h2 className="text-gray-100 
+                    text-heading-lg"
+                >
+                    Venda para seu público através de 
+                    uma plataforma única
+                </h2>
+            </div>
+            <div className="col-span-full
+                flex 
+                flex-col 
+                gap-2"
+            >
+                <div className="
+                    grid grid-cols-1 md:grid-cols-2
+                    gap-12 md:gap-4 rounded-lg  bg-gray-500 
+                    p-4 md:p-12"
+                > 
+                    <div className="flex flex-col 
+                        gap-4 "
+                    >
+                        <span className="text-body-tag
+                            text-blue-200 bg-blue-400 px-2 py-1
+                            w-fit rounded-sm uppercase"
+                        >
+                            Personalizável
+                        </span>
+                        <h2 className="text-gray-100 
+                            text-heading-lg"
+                        >
+                            Tenha uma loja online personalizada 
+                            com a cara da sua marca 
+                        </h2>
+                        <Button asChild  className="
+                            w-fit
+                            rounded-full 
+                            mt:4 
+                            md:mt-auto md:flex
+                            hidden "
+                        >
+                            <Link href="/criar-loja">
+                                Criar loja grátis
+                                <ArrowRight />
+                            </Link>
+                        </Button>
+                    </div>
+                    <div className="flex 
+                        flex-col 
+                        itens-center
+                        justify-center
+                        w-full"
+                    >
+                        <div className="w-full
+                            max-w-md 
+                            overflow-hidden"
+                        >
+                            <Image 
+                                src="/feature-section.svg"
+                                alt="feature"
+                                width={440}
+                                height={330}
+                                className="object-cover
+                                w-full"
+                            />
+                        </div>
+                        <Button asChild  className="
+                            rounded-full 
+                            mt:4 
+                            md:mt-auto
+                            w-full 
+                            gap-2 
+                            md:hidden"
+                        >
+                            <Link href="/criar-loja">
+                                Criar loja grátis
+                                <ArrowRight />
+                            </Link>
+                        </Button>
+                    </div>
+                </div>
+           </div>
+
+        </section>
+    )
+}
