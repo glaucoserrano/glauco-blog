9c2f9b164c2a07a2ec36985c8d5f86ae7919fc8a
Glauco Gamero <glauco.h.gamero@gmail.com>
2025-06-24 07:36:21 -0300

    add not-found page

--- src/components/search/search.tsx
+++ src/components/search/search.tsx
@@ -1,12 +1,14 @@
 import { cn } from "@/lib/utils";
 import { CircleX, SearchIcon } from "lucide-react";
 import { useRouter, useSearchParams } from "next/navigation";
-import { useCallback } from "react";
+import { useCallback, useEffect, useRef } from "react";
 
 export const Search = () => {
-  const router = useRouter();
-  const searchParams = useSearchParams();
-  const query = searchParams?.get('q') ?? '';
+  const inputRef = useRef<HTMLInputElement | null>(null)
+  const router = useRouter()
+  const searchParams = useSearchParams()
+  const query = searchParams?.get('q') ?? ''
+  const hasQuery = !!searchParams?.has('q')
 
   const handleSearch = useCallback(
     (event: React.FormEvent) => {
@@ -30,6 +32,11 @@ export const Search = () => {
       scroll: false
     })
   }
+  useEffect(() => {
+    if(hasQuery){
+      inputRef.current?.focus()
+    }
+  },[hasQuery])
   return (
     <form onSubmit={handleSearch} className="relative group w-full md:w-60">
       <SearchIcon
@@ -39,6 +46,7 @@ export const Search = () => {
         )}
       />
       <input
+        ref={inputRef}
         onChange={handleQueryChange}
         type="text"
         value={query}
