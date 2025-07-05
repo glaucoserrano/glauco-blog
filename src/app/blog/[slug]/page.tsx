f58ad27f3c3c17ef9169fd53ab21df9d867aa46d
Glauco Gamero <glauco.h.gamero@gmail.com>
2025-07-05 09:59:57 -0300

    add seo dinymics

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,12 +1,35 @@
 import { PostPage } from "@/templates/blog";
 import { allPosts } from "contentlayer/generated";
+import type { Metadata } from "next";
 import { notFound } from "next/navigation";
 
-type BlogPostPage ={
+
+type BlogPostPageProps ={
     params: Promise<{
         slug: string;
     }>
 }
+export async function generateMetadata({params}:BlogPostPageProps): Promise<Metadata> {
+    const {slug} = await params
+
+    const post = allPosts.find((post) => post.slug === slug)
+
+    if(!post){
+        return {}
+    }
+    return{
+        title:post.title,
+        description: post.description,
+        authors: [{
+            name: post.author.name,
+        }],
+        robots:'index,follow',
+        openGraph:{
+            images:[post.image]
+        }
+    }
+}
+
 export const revalidate = 60
 export const dynamicParams = true
 export async function generateStaticParams(){
@@ -15,7 +38,7 @@ export async function generateStaticParams(){
     }))
 }
 
-export default async function BlogPostPage({ params } : BlogPostPage) {
+export default async function BlogPostPage({ params } : BlogPostPageProps) {
     const {slug} = await params
     const post = allPosts.find((post) => post?.slug === slug)
     if(!post) {
